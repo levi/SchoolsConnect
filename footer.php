1ccd6c42d967ad32eb2d035d6924646ade6f0010
stillmotion <levi@levimccallum.com>
2011-09-14 16:02:10 -1000

    Added JSON2.js

--- footer.php
+++ footer.php
@@ -35,6 +35,7 @@
 
 		<?php if (is_user_logged_in()): ?>
 		<script src="<?php bloginfo('stylesheet_directory') ?>/js/backbone.js"></script>
+		<script src="<?php bloginfo('stylesheet_directory') ?>/js/libs/json2.js"></script>
 		<?php
 			function load_backbone($type)
 			{
